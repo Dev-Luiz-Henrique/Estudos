d4a421ce9c11611a69cb47728bbc9d1530705038
Luiz Henrique de Santana <123594208+Dev-Luiz-Henrique@users.noreply.github.com>
2024-01-16 15:05:58 -0300

    Adição de Exercícios Educativos
    Adição dos exercícios educativos sobre a criação de funções em linguagem c, repartindo o código em funções que podem ser chamadas a qualquer momento, retornando ou não valores específicos, além de explorar recursão, evitando uso de laços no programa.

---
+++ Conteudo/05 - Funcoes/EX03 - Prototipo de Funcao.c
@@ -0,0 +1,18 @@
+// Crie um programa que use um prototipo de funcao (Declarada apos o main)
+
+#include <stdio.h>
+
+// Prototipo da fun��o
+void funcao();
+
+int main() {
+    // Chamando a funcao
+    funcao();
+    
+    return 0;
+}
+
+// Definicao da funcao
+void funcao() {
+    printf("Esta eh uma funcao declarada abaixo da funcao main.\n");
+}
---
+++ Conteudo/05 - Funcoes/EX09 - Recursao Inversao de Numeros.c
@@ -0,0 +1,26 @@
+// Codifique uma Funcao Recursiva que recebe um natural n e
+// imprime seus digitos invertidos (da direita para a esquerda).
+
+#include <stdio.h>
+
+// Funcao recursiva que imprime os digitos de n ao contrario
+void inverte(unsigned n)
+{
+	if (n<10)	
+		printf("%u", n);
+	else {
+		printf("%u", n%10);
+		inverte(n/10);
+	}
+}
+
+int main(){
+    unsigned int n;
+    printf("Digite n: ");
+    scanf ("%u", &n);
+
+	// Imprime o valor de n ao contrario
+    printf("O valor de %u ao contrario eh ", n);
+    inverte(n);
+	return 0;
+}
